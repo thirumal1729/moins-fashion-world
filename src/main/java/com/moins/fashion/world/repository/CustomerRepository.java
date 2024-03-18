4fe139a5a348708d800e63120089d1252109a88b
DEBASHIS559 <debashisganguly321@gmail.com>
2024-03-18 17:33:12 +0530

    Updated customer controller

--- src/main/java/com/moins/fashion/world/repository/CustomerRepository.java
+++ src/main/java/com/moins/fashion/world/repository/CustomerRepository.java
@@ -1,5 +1,12 @@
 package com.moins.fashion.world.repository;
 
-public interface CustomerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.moins.fashion.world.entity.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer>{
+
+	Customer findbyEmailandPassword(String email, String password);
+	
+	
 }
