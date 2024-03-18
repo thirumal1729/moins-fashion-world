ea863256556df00531632303f553d91a667f4009
Kaviyakrk <kaviyakrk123@gmail.com>
2024-03-18 17:20:14 +0530

    save admin done

--- src/main/java/com/moins/fashion/world/service/AdminService.java
+++ src/main/java/com/moins/fashion/world/service/AdminService.java
@@ -1,6 +1,7 @@
 package com.moins.fashion.world.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
@@ -10,6 +11,7 @@ import com.moins.fashion.world.dao.AdminDao;
 import com.moins.fashion.world.dto.ResponseStructure;
 import com.moins.fashion.world.entity.Admin;
 import com.moins.fashion.world.exception.ValidationException;
+import com.moins.fashion.world.payload.AdminDto;
 
 @Service
 public class AdminService {
@@ -17,15 +19,24 @@ public class AdminService {
 	private AdminDao adminDao;
 
 	// Save Admin
-//	public ResponseEntity<ResponseStructure<Admin>> saveAdmin(Admin admin, BindingResult result) {
-//		if (result.hasErrors()) {
-//			String message = "";
-//			for (FieldError error : result.getFieldErrors()) {
-//				message = message + error.getDefaultMessage() + "\n";
-//			}
-//			throw new ValidationException(message);
-//		}
-//		Admin adminObject=new Admin();
-//		
-//	}
+	public ResponseEntity<ResponseStructure<Admin>> saveAdmin(AdminDto adminDto, BindingResult result) {
+		if (result.hasErrors()) {
+			String message = "";
+			for (FieldError error : result.getFieldErrors()) {
+				message = message + error.getDefaultMessage() + "\n";
+			}
+			throw new ValidationException(message);
+		}
+		 Admin adminObject=Admin.builder().adminName(adminDto.getAdminName()).email(adminDto.getEmail()).password(adminDto.getPassword())
+				.phone(adminDto.getPhone()).build();
+
+		adminObject = adminDao.saveAdmin(adminObject);
+
+		ResponseStructure<Admin> responseStructure = new ResponseStructure<Admin>();
+		responseStructure.setStatusCode(HttpStatus.CREATED.value());
+		responseStructure.setMessage("Created");
+		responseStructure.setData(adminObject);
+
+		return new ResponseEntity<ResponseStructure<Admin>>(responseStructure,HttpStatus.CREATED);
+	}
 }
